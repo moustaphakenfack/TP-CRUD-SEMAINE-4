a3b6fbe07c14e508b5a6b05d06d955f363e73106
moustaphakenfack <dagoberdagober81@gmail.com>
2026-07-23 16:50:19 +0100

    mon crud terminer

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Laravel 12 CRUD Application </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" crossorigin="anonymous">
+</head>
+<body>   
+
+    <div class="container">
+        <h3 class=" mt-3">Laravel 12 CRUD Application <a href="https:">NUMERIX.COM</a></h3>
+            @yield('content')
+            <div class="row justify-content-center text-center mt-3">
+                <div class="col-md-12">
+                    <p>Back to Tutorial: 
+                        <a href="https://www.numerix.com/simple-laravel-12-crud-application-tutorial/"><strong>STAGE DE FORMATION</strong></a>
+                    </p>
+                    <p>
+                        For More Web Development Tutorials Visit: <a href="https://www.numerix.com/"><strong>NUMERIX.COM</strong></a>
+                    </p>
+                </div>
+            </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
